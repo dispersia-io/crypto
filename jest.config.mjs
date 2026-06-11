c9e05b5bf82bda12327185635f125c61cff3cb45
Oleg Putseiko <oleg.putseiko@gmail.com>
2026-06-12 01:43:14 +0900

    test(translator): add translator test cases

---
+++ jest.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+};
